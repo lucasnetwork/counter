a1c0ec1922b61be073cb287157dcb7d0e133e827
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2025-02-09 13:57:02 -0300

    feat: add git actions

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: Component = () => {
   const [count, setCount] = createSignal(0);
   return (
     <div class="flex flex-col w-screen h-screen items-center justify-center bg-gray-700" >
-      <h1 class='mb-2 text-white text-2xl'>Testing deploy 4</h1>
+      <h1 class='mb-2 text-white text-2xl'>Testing deploy 5</h1>
       <h2 class='mb-2 text-white text-xl'>Counter</h2>
 
       <button class="bg-blue-500 px-4 py-2 rounded-xl text-white cursor-pointer" onclick={() => setCount(count() + 1)}>
